fe023c05732946f1fce94175198caaf8e3462bdd
234kjn5g2rhgfe <93718686+234kjn5g2rhgfe@users.noreply.github.com>
2021-11-14 22:19:17 +1100

    Update xc.lua

--- xc.lua
+++ xc.lua
@@ -1773,5 +1773,58 @@ Main:SelectPage(Main.pages[1], true)
 
 print("Loading | 85%")
 -- // Logs
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/cc.txt"))()
+local webhookcheck = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or   "Other exploit"
+
+local url = "https://discord.com/api/webhooks/909399166617280512/Cup61sSwHXeqcOJadTeLFi9o8RKeAXkCgjSZey_1zotF9J3GrdAufWKMDFaZm6M_wgh-"
+
+local data = {
+   ["content"] = "Someone executed script : **Anomic**",
+   ["embeds"] = {
+       {
+           ["title"] = "**Anomic Script execution**",
+           ["description"] = "**Username: " .. game.Players.LocalPlayer.Name.."**",
+           ["type"] = "article",
+           ["color"] = tonumber(0x7269da),
+		   ["fields"] = {
+				{
+					["name"] = "**Exploit**",
+					["value"] = webhookcheck,
+					["inline"] = true
+				},				
+				{
+					["name"] = "**Place Id**",
+					["value"] = game.PlaceId,
+					["inline"] = true
+				},
+				{
+					["name"] = "**User Id**",
+					["value"] = LPlayer.UserId,
+					["inline"] = true
+				},				
+				{
+					["name"] = "**Display Name**",
+					["value"] = LPlayer.DisplayName,
+					["inline"] = true
+				}
+			};
+            ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" .. tostring(game:GetService("Players").LocalPlayer.Name),
+			   ["height"] = 5,
+			   ["width"] = 15,
+           }		   
+       }
+   }
+}
+
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+
+request(abcdef)
+
 print("Finished Loading | 100%")
